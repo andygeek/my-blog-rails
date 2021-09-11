1242441633b783fe5b24f54a19d0743f35f220aa
Andy Eulogio <andy.eulogio@gmail.com>
2021-09-11 12:19:26 -0500

    Add test for relationship in user and post

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe User, type: :model do
       should validate_presence_of(:name)
       should validate_presence_of(:auth_token)
     end
+
+    it "validate relationship" do
+      should have_many(:posts)
+    end
   end
 end
