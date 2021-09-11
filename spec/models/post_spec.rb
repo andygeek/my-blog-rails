fd6c15be9066500ea784f5dab23d57ab8e5a12d6
Andy Eulogio <andy.eulogio@gmail.com>
2021-09-11 12:10:40 -0500

    Add test for user model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-
-  # Este pending es autogenerado, y sirve para que muestre el error al no encontrar nada
-  pending "add some examples to (or delete) #{__FILE__}"
-
   describe "validations" do
     it "validate presence of required fields" do
       should validate_presence_of(:title)
@@ -13,5 +9,4 @@ RSpec.describe Post, type: :model do
       should validate_presence_of(:user_id)
     end
   end
-  
 end
