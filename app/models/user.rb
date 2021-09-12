f2f0c6d4ddbbfd9665987d9d242e2e3e0ff0c2aa
Andy Eulogio <andy.eulogio@gmail.com>
2021-09-12 14:11:29 -0500

    Add TokenGenerate

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :name, presence: true
   validates :auth_token, presence: true
+
+  after_initialize :generate_auth_token
+
+  def generate_auth_token
+    # User.new
+    unless auth_token.present?
+      # generate token
+      self.auth_token = TokenGenerationService.generate
+    end
+  end
 end
